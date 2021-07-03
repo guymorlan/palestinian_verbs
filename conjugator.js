4b2c84039b9bcab4bb93315f75da3a0ba1316394
Guy Mor-Lan <44950985+guymorlan@users.noreply.github.com>
2021-07-03 23:14:47 +0300

    Add files via upload

--- conjugator.js
+++ conjugator.js
@@ -3,7 +3,8 @@
 var data = $.ajax({
 	 // url: "https://rawcdn.githack.com/guymorlan/levantine_verbs/f7f40850b714356e8fac6694192b9f6cdfd15797/data.json",
 	// url: "https://rawcdn.githack.com/guymorlan/levantine_verbs/c2cf6e63276c7873156d4205482d06553879c1ca/data_1906.json",
-	 url: "https://raw.githack.com/guymorlan/palestinian_verbs/main/data_new.json",
+	 //url: "https://raw.githack.com/guymorlan/palestinian_verbs/main/data_new.json",
+	 url: "https://raw.githack.com/guymorlan/palestinian_verbs/main/data_0307c.json",
   async: false,
   dataType: 'json'
 });
@@ -13,21 +14,43 @@ function updateInfo(id){
 	var info = document.getElementsByName("verb-info");
 	info[0].innerHTML = "<b>Verb: </b>" + data["responseJSON"][id]["verb"] + "<br>" +
 	//"<b> Root: </b>" + data["responseJSON"][id]["root"] + "<br>" +
-	"<b> Form: </b>" + data["responseJSON"][id]["form"] + "(" + data["responseJSON"][id]["form_type"] + ")" + "<br>" +
+	"<b> Form: </b>" + data["responseJSON"][id]["form"] + " (" + data["responseJSON"][id]["form_type"] + ")" + "<br>" +
 	"<b> Meaning: </b>" + data["responseJSON"][id]["english"] + "<br>" +
 	"<b> Comments: </b>" + data["responseJSON"][id]["explanation"]
-
-	
-	
 }
-var gridMain = new gridjs.Grid({
+
+var gridEnMain = new gridjs.Grid({
 	columns: ["Person", "Present Future", "Past"], 
-	data: data["responseJSON"][0]["main"],
+	data: data["responseJSON"][0]["enMain"],
 	width: "50%",
 	style: {table: {
       'font-size': '25px'
-	  }}}).render(document.getElementById("main-table"));
+	  }}}).render(document.getElementById("en-main-table"));
 	
+	
+var gridEnOther = new gridjs.Grid({
+	columns: ["Type", "Value"], 
+	data: data["responseJSON"][0]["enOther"],
+	width: "50%",
+	style: {table: {
+      'font-size': '25px'
+	  }}}).render(document.getElementById("en-other-table"));
+	  
+var gridHeMain = new gridjs.Grid({
+	columns: ["Person", "Present Future", "Past"],
+	data: data["responseJSON"][0]["heMain"],
+	width: "50%",
+	style: {table: {
+      'font-size': '25px'
+	  }}}).render(document.getElementById("he-main-table"));
+
+var gridHeOther = new gridjs.Grid({
+	columns: ["Type", "Value"], 
+	data: data["responseJSON"][0]["heOther"],
+	width: "50%",
+	style: {table: {
+      'font-size': '25px'
+	  }}}).render(document.getElementById("he-other-table"));
 
 $('.js-example-basic-single').on('select2:select', function (e) {
 	
@@ -35,7 +58,11 @@ $('.js-example-basic-single').on('select2:select', function (e) {
 	//$("#user-table").html("");
 
 	//console.log(data[e.params.data.id])
-	gridMain.updateConfig({columns: ["Person", "Present Future", "Past"], data: data["responseJSON"][e.params.data.id]["main"]}).forceRender();
+	gridEnMain.updateConfig({columns: ["Person", "Present Future", "Past"], data: data["responseJSON"][e.params.data.id]["enMain"]}).forceRender();
+	gridHeMain.updateConfig({columns: ["Person", "Present Future", "Past"], data: data["responseJSON"][e.params.data.id]["heMain"]}).forceRender();
+	gridEnOther.updateConfig({columns: ["Type", "Value"], data: data["responseJSON"][e.params.data.id]["enOther"]}).forceRender();
+	gridHeOther.updateConfig({columns: ["Type", "Value"], data: data["responseJSON"][e.params.data.id]["heOther"]}).forceRender();
+
 
 	updateInfo(e.params.data.id);
 	//.render(document.getElementById("user-table"));
